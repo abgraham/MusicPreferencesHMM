619e033faad1e0b5007fd839fac35a415e95ff4b
Annie Graham <abgraham@stanford.edu>
2017-11-29 07:10:14 -0800

    First commit

---
+++ song_data_parser.py
@@ -0,0 +1,31 @@
+
+# we want this to take in the big file of user data
+# to make it just into a list of song IDs
+# and then to make another file that's just a list of user ID's split up by spaces (so that the webppl file can use the .split() method on it)
+
+file = open('user1data.txt')
+listOfSongIDs = []
+for line in file:
+    #print (line.split())[2]
+    songID = (line.split())[-1]
+    listOfSongIDs.append(songID)
+
+listOfUniqueIDs = set(listOfSongIDs)
+
+print listOfSongIDs
+stringOfIDs = ''
+for song in listOfSongIDs:
+    stringOfIDs += (song + ' ')
+
+print 'final string: ' + stringOfIDs
+# TODO: make this specific to the file it had been before
+file = open('parsed_user_data.txt', 'w')
+for song in listOfSongIDs:
+    file.write(song + ' ')
+file.close()
+
+file = open('parsed_unique_user_data.txt', 'w')
+for song in listOfUniqueIDs:
+    file.write(song + ' ')
+file.close()
+
